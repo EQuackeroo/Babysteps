a9c7d6c70b09e8bf52bb97ff495a1aa83f39a8d8
ZeronTheXeon <apgthexeon@gmail.com>
2015-09-05 20:25:41 -0700

    Created Solution

---
+++ BranchedWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * User: MasterJain
+ * Date: 9/5/2015
+ * Time: 8:24 PM
+ */
+using System;
+
+namespace BranchedWorld
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
